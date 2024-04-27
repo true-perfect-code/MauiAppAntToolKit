b8ac069a4a7694591c3e5d6a07a337d3895a8814
sascha <sascha.simic@mail.com>
2024-04-27 19:15:42 +0200

    Projektdateien hinzufügen.

---
+++ MauiAppAntToolKit/MauiProgram.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui.Core;
+
+namespace MauiAppAntToolKit
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            //builder
+            //    .UseMauiApp<App>()
+            //    .ConfigureFonts(fonts =>
+            //    {
+            //        fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            //    });
+            builder
+                .UseMauiApp<App>()
+                // Initialize the .NET MAUI Community Toolkit by adding the below line of code
+                .UseMauiCommunityToolkitCore()
+                // After initializing the .NET MAUI Community Toolkit, optionally add additional fonts
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+            builder.Services.AddMauiBlazorWebView();
+
+            // Ant
+            builder.Services.AddAntDesign();
+
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
